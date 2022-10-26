03dc3ae6e37188e11af00e16612d076b8daa800c
Dani Ihza Farrosi <dani.videda@gmail.com>
2022-10-26 17:22:39 +0700

    add logger on Main.ts (WIP)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = 3333
+
 async function bootstrap() {
+  const logger = new Logger('Main (Main.ts)')
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/v0');
   app.useGlobalPipes(
@@ -10,6 +13,7 @@ async function bootstrap() {
       whitelist: true /**  This will whitelist the DTO fields */,
     }),
   );
-  await app.listen(3333);
+  await app.listen(PORT);
+  logger.log(`Server started on port ${PORT} `)
 }
 bootstrap();
