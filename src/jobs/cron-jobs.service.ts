da8bc6130ff75abac8bed7286c71d0ed03ad81ec
Dani Ihza Farrosi <dani.videda@gmail.com>
2022-12-06 13:45:06 +0700

    handle cron jobs for batching (WIP)

--- src/jobs/cron-jobs.service.ts
+++ src/jobs/cron-jobs.service.ts
@@ -7,16 +7,33 @@ import { REDIS_CLIENT } from '../common';
 export class CronJobsService {
   private readonly logger = new Logger(CronJobsService.name);
 
-  constructor(
-    @Inject(REDIS_CLIENT) private readonly redisClient: RedisClientType,
-  ) {}
+  constructor(@Inject(REDIS_CLIENT) private readonly redisClient: RedisClientType) {}
 
-  @Cron(CronExpression.EVERY_5_SECONDS)
+  @Cron(CronExpression.EVERY_10_SECONDS)
   async handleCron() {
-    const transaction = await this.redisClient.json.GET(
-      'Transactions:stake_test1qra6ryfvycs4gf0wa42qak5rvv2rs8u85ru76zgganmwptp0d5dxpdatxft8ka436d8z4765fvacmdcxv7kjss08sg8q08scm7',
-    );
+    const transactionQueueKey = 'Transactions:Queue';
+    const batchesRepoKey = 'Batches:';
 
-    this.logger.log('Cron job handled every 5 second: ', transaction);
+    let count = await this.redisClient.lLen(transactionQueueKey);
+    let transactionIdList: Array<string> = [];
+    let transactionObjList = [];
+    let batchedTransaction = new Map();
+
+    if (count < 3) {
+      return this.logger.log(`Not enough transaction in Queue. Current: ${count}`);
+    }
+
+    transactionIdList = await this.redisClient.lRange(transactionQueueKey, 0, 3);
+
+    transactionIdList.forEach(async (transactionId) => {
+      const transactionObj = await this.redisClient
+        .multi()
+        .json.get(transactionId.toString())
+        .json.del(transactionId.toString())
+        .lPop(transactionQueueKey)
+        .exec();
+    });
+
+    this.logger.log(`Cron job handled every 10 seconds: `);
   }
 }
