e93ac33bf60f20e74447986ec106d625d3210f18
Dani Ihza Farrosi <dani.videda@gmail.com>
2023-09-25 08:36:52 +0700

    formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { IBM_Plex_Serif } from 'next/font/google';
+
+const ibm = IBM_Plex_Serif({
+  weight: '500',
+  subsets: ['latin'],
+});
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="container ml-5 mr-5">
+      <h1 className={`${ibm.className} text-lg`}>lowfee.</h1>
     </div>
   );
 }
