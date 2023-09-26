1191077706bb13dc1fd62a5947488d7f370eb6e1
Dani Ihza Farrosi <dani.videda@gmail.com>
2023-09-26 17:34:47 +0700

    add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,30 @@
 import { IBM_Plex_Serif } from 'next/font/google';
 
-const ibm = IBM_Plex_Serif({
+const ibm_500 = IBM_Plex_Serif({
   weight: '500',
+  subsets: ['latin'],
+});
+
+const ibm_700 = IBM_Plex_Serif({
+  weight: '700',
   subsets: ['latin'],
 });
 
 export default function Home() {
   return (
-    <div className="container ml-5 mr-5">
-      <h1 className={`${ibm.className} text-lg`}>lowfee.</h1>
+    <div className="container mx-auto py-2 min-h-screen">
+      <div className="flex flex-col min-h-screen justify-center items-center">
+        <div className="w-1/2">
+          <h2
+            className={`${ibm_500.className} text-2xl text-center text-primary`}
+          >
+            lowfee.
+          </h2>
+          <h1 className={`${ibm_700.className} text-5xl text-center`}>
+            Lower your transaction fees on Cardano.
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
