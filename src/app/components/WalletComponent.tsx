bd98403509ea981cdb5290cf291fa7e1a446ed86
Dani Ihza Farrosi <dani.videda@gmail.com>
2024-01-22 17:44:56 +0700

    add address information to WalletComponent

--- src/app/components/WalletComponent.tsx
+++ src/app/components/WalletComponent.tsx
@@ -5,21 +5,26 @@ import * as cbor from 'cbor';
 
 export default function WalletComponent() {
   const [balance, setBalance] = useState(null);
+  const [userAddress, setUserAddress] = useState('');
   const [buttonState, setButtonState] = useState('');
 
   async function handleClick() {
     setButtonState('connecting');
     const walletApi = await window.cardano.eternl.enable();
     const balance = await walletApi.getBalance();
+    const userAddress = (await walletApi.getRewardAddresses())[0];
+
     setBalance(cbor.decode(balance)[0]);
+    setUserAddress(userAddress);
   }
 
   if (balance !== null) {
     return (
       <>
-        <span className="mx-auto mt-5 w-full bg-gray-100 rounded border border-primary p-2">
-          Balance: {balance}
-        </span>
+        <div className="mx-auto mt-5 w-full bg-gray-100 rounded border border-primary p-2">
+          <div>Balance: {balance}</div>
+          <div className="break-words">Address: {userAddress}</div>
+        </div>
       </>
     );
   } else {
