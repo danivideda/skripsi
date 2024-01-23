d27d59bbb86b17d18d15a68646ed720259d5e7ee
Dani Ihza Farrosi <dani.videda@gmail.com>
2024-01-23 13:12:19 +0700

    implement disconnect wallet

--- src/app/components/WalletComponent.tsx
+++ src/app/components/WalletComponent.tsx
@@ -1,26 +1,49 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import * as cbor from 'cbor';
 
 export default function WalletComponent() {
-  const [balance, setBalance] = useState(null);
+  const [balance, setBalance] = useState('');
   const [userAddress, setUserAddress] = useState('');
   const [buttonState, setButtonState] = useState('');
 
-  async function handleClick() {
-    setButtonState('connecting');
+  async function handleClickConnectWallet() {
+    setButtonState('loading');
     const walletApi = await window.cardano.eternl.enable();
     const balance = await walletApi.getBalance();
     const userAddress = (await walletApi.getRewardAddresses())[0];
 
     setBalance(cbor.decode(balance)[0]);
     setUserAddress(userAddress);
+    setButtonState('');
   }
 
-  if (balance !== null) {
+  async function handleClickDisconnectWallet() {
+    setButtonState('loading');
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    setBalance('');
+    setUserAddress('');
+    setButtonState('');
+  }
+
+  if (balance !== '') {
     return (
       <>
+        <button
+          type="button"
+          disabled={buttonState === 'loading'}
+          className={
+            'mx-auto mt-5 w-full bg-red-300 rounded border border-primary p-2' +
+            ' ' +
+            (buttonState === 'loading' ? 'text-white' : '')
+          }
+          onClick={handleClickDisconnectWallet}
+        >
+          {buttonState === 'loading'
+            ? 'Disconnecting wallet...'
+            : 'Disconnect Wallet'}
+        </button>
         <div className="mx-auto mt-5 w-full bg-gray-100 rounded border border-primary p-2">
           <div>Balance: {balance}</div>
           <div className="break-words">Address: {userAddress}</div>
@@ -29,22 +52,18 @@ export default function WalletComponent() {
     );
   } else {
     return (
-      <>
-        <button
-          type="button"
-          disabled={buttonState === 'connecting'}
-          className={
-            'mx-auto mt-5 w-full bg-gray-100 rounded border border-primary p-2' +
-            ' ' +
-            (buttonState === 'connecting' ? 'text-gray-300' : '')
-          }
-          onClick={handleClick}
-        >
-          {buttonState === 'connecting'
-            ? 'Connecting wallet...'
-            : 'Connect Wallet'}
-        </button>
-      </>
+      <button
+        type="button"
+        disabled={buttonState === 'loading'}
+        className={
+          'mx-auto mt-5 w-full bg-gray-100 rounded border border-primary p-2' +
+          ' ' +
+          (buttonState === 'loading' ? 'text-gray-300' : '')
+        }
+        onClick={handleClickConnectWallet}
+      >
+        {buttonState === 'loading' ? 'Connecting wallet...' : 'Connect Wallet'}
+      </button>
     );
   }
 }
