82ab2ee434dac27eaa5ed08edaa040e16ee1ed7c
Dani Ihza Farrosi <dani.videda@gmail.com>
2024-05-13 10:19:15 +0700

    change wording

--- src/app/components/landing-page/heading-skripsi.tsx
+++ src/app/components/landing-page/heading-skripsi.tsx
@@ -17,15 +17,16 @@ export default function HeadingSkripsi() {
         <h2
           className={`${ibm_500.className} text-2xl text-center text-primary mb-2`}
         >
-          implementasi.
+          skripsi.
         </h2>
       </div>
       <div className="w-[650px] min-h-full text-neutral-700">
         <h1 className={`${ibm_700.className} text-5xl text-center mb-2`}>
-        Skripsi
+          Agregasi Transaksi
         </h1>
         <p className="text-center text-base">
-          aplikasi agregasi transaksi untuk mengurangi biaya transaksi di Cardano.
+          implementasi agregasi transaksi untuk mengurangi biaya transaksi di
+          Cardano.
         </p>
       </div>
     </>
