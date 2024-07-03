de7ab20fcea8d3ceba2096da74b4968100f9e592
Dani Ihza Farrosi <dani.videda@gmail.com>
2024-07-03 10:51:33 +0700

    change refreshing implementation for queue

--- src/app/transaction-aggregation-queue/TransactionQueue.tsx
+++ src/app/transaction-aggregation-queue/TransactionQueue.tsx
@@ -1,17 +1,32 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { truncate } from '../helper';
 import { NumericFormat } from 'react-number-format';
+import { WalletContext } from '../wallet-provider';
 
 export default function TransactionQueue() {
-  const [queueList, setQueueList] = useState({} as any);
+  const [queueList, setQueueList] = useState<any>({});
+  const [isFetching, setIsFetching] = useState(false);
+  const walletContext = useContext(WalletContext);
 
   useEffect(() => {
+    // first time fetch
     fetchQueue();
-  }, []);
+
+    // interval fetch every 5 sec
+    const intervalFetch = setInterval(() => {
+      fetchQueue();
+      console.log('refreshed');
+    }, 6 * 1000);
+
+    return () => {
+      clearInterval(intervalFetch);
+    };
+  }, [walletContext.walletStatus]);
 
   async function fetchQueue() {
+    setIsFetching(true);
     const url = `${process.env.backendUrl}/transactions/queue`;
     const response = await fetch(url, {
       method: 'GET',
@@ -19,16 +34,19 @@ export default function TransactionQueue() {
     });
     const body = await response.json();
     setQueueList(body);
+    setTimeout(() => {
+      setIsFetching(false);
+    }, 500);
   }
 
   return (
     <div className="w-full">
-      {!queueList.queue_list ? 'loading' : <QueueList list={queueList} />}
+      {!queueList.queue_list ? 'loading' : <QueueList list={queueList} isFetching={isFetching} />}
     </div>
   );
 }
 
-function QueueList({ list }: { list: any }) {
+function QueueList({ list, isFetching }: { list: any; isFetching: boolean }) {
   type transactionResponseObj = {
     stakeAddress: string;
     utxos: string[];
@@ -40,10 +58,13 @@ function QueueList({ list }: { list: any }) {
     <>
       <h1 className="text-lg w-full text-center">Transaction Aggregation Queue</h1>
       <div>
-        <h1 className="font-semibold">Number of queue: {list.queue_list.length}</h1>
+        <div className="flex justify-between">
+          <h1 className="font-semibold">Number of queue: {list.queue_list.length}</h1>
+          {isFetching && <h1 className="font-light italic">Fetching...</h1>}
+        </div>
         <table className="w-full border-2 border-black mt-2">
           <thead>
-            <tr>
+            <tr className="bg-orange-200">
               <th className="border-y-2 border-x border-black p-1">No.</th>
               <th className="border-y-2 border-x border-black p-1">User</th>
               <th className="border-y-2 border-x border-black p-1">Destination</th>
@@ -52,7 +73,12 @@ function QueueList({ list }: { list: any }) {
           </thead>
           <tbody className="font-normal">
             {list.queue_list.length === 0 ? (
-              <tr>Queue empty</tr>
+              <tr className="bg-gray-200">
+                <td className="p-1 px-3 text-center border border-black break-words"></td>
+                <td className="p-1 px-3 text-center border border-black break-words"></td>
+                <td className="p-1 px-3 text-center border border-black break-words"></td>
+                <td className="p-1 px-3 text-center border border-black break-words"></td>
+              </tr>
             ) : (
               list.queue_list.map((item: transactionResponseObj, index: number) => {
                 return (
